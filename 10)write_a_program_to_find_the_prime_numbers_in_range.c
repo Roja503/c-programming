75cfed5c9ce780f93a44235db158bf8a5fe36066
Roja503 <146074998+Roja503@users.noreply.github.com>
2024-07-14 19:39:03 +0530

    Create 10)write_a_program_to_find_the_prime_numbers_in_range.c

---
+++ 10)write_a_program_to_find_the_prime_numbers_in_range.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main() {
+   int a,b,i,j,count;
+   scanf("%d%d",&a,&b);
+   for(i=a;i<=b;i++)
+   {
+     count=0;
+     for(j=1;j<=i;j++)
+     {
+     if(i%j==0)
+     count++;
+     }
+   if(count==2)
+   printf("%d",i);
+   } 
+}
