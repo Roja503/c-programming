6e6122fc2e8e2e6687b7762a22f72efcf2a7fff9
Roja503 <146074998+Roja503@users.noreply.github.com>
2024-07-19 09:51:25 +0530

    Create GCD_of_2_numbers.c

---
+++ GCD_of_2_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+   int a=3,b=6,max;
+   max=(a>b)?a:b;
+   if(max % a == 0 && max % b == 0)
+   {
+       printf("LCM %d,%d,%d",a,b,max);
+    }
+    return 0;
+}
